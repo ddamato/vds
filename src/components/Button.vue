b7537a04ee5d1891fb6c61cd6d7fa02039ecb28b
Donnie D'Amato <fauxserious@gmail.com>
2020-10-03 13:56:44 -0400

    Working example

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template functional>
+  <button v-bind="data.attrs" v-on="listeners"><slot/></button>
+</template>
+
+<style scoped>
+  button {
+    background-color: blue;
+    color: white;
+  }
+</style>
